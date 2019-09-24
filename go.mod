e250d485426a3e10fcc2256040ef1c50c3001283
Jason Shepherd <jshepher@redhat.com>
2019-09-24 16:28:53 +1000

    trying to use buildah to pull image, then get machine-os-conent image ref system contents

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/jasinner/rhcos-tracker
+
+go 1.12
+
+require (
+	github.com/containers/buildah v1.11.2
+	github.com/containers/image v3.0.2+incompatible
+	github.com/containers/storage v1.13.2
+	k8s.io/klog v0.3.1
+)
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"os/exec"
+
+	"github.com/containers/buildah"
+	"github.com/containers/buildah/pkg/unshare"
+	"github.com/containers/image/types"
+	"github.com/containers/storage"
+	"k8s.io/klog"
 )
 
 func getRPMs(w http.ResponseWriter, r *http.Request) {
@@ -16,19 +21,42 @@ func getRPMs(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Please supply a tag parameter", http.StatusBadRequest)
 		return
 	}
-	imageTag := fmt.Sprintf("quay.io/openshift-release-dev/ocp-release:%s", tag)
-	fmt.Fprintf(w, "Getting RPMs for image %s", imageTag)
-	cmd := exec.Command("/usr/bin/oc", "adm", "release", "info", imageTag)
-	var stderr bytes.Buffer
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	if err != nil {
-		log.Println(fmt.Sprint(err) + ": " + stderr.String())
-		return
+	image := fmt.Sprintf("quay.io/openshift-release-dev/ocp-release:%s", tag)
+	fmt.Fprintf(w, "Getting RPMs for image %s", image)
+
+	buildStoreOptions, err := storage.DefaultStoreOptions(unshare.IsRootless(), unshare.GetRootlessUID())
+	buildStore, err := storage.GetStore(buildStoreOptions)
+
+	builderOpts := buildah.BuilderOptions{
+		FromImage:        image,                   // Starting image
+		Isolation:        buildah.IsolationChroot, // Isolation environment
+		CommonBuildOpts:  &buildah.CommonBuildOptions{},
+		ConfigureNetwork: buildah.NetworkDefault,
+		SystemContext:    &types.SystemContext{},
 	}
-	io.WriteString(w, out.String())
+
+	// getContext returns a context.TODO
+	builder, err := buildah.NewBuilder(getContext(), buildStore, builderOpts)
+	if err != nil {
+		log.Println(fmt.Errorf("error creating buildah builder: %v", err))
+	}
+
+	mountPath, err := builder.Mount("")
+	defer func() {
+		err := builder.Unmount()
+		if err != nil {
+			klog.Errorf("failed to unmount: %v", err)
+		}
+	}()
+	if err != nil {
+		log.Println(fmt.Errorf("error mounting image content from image %s: %v", image, err))
+	}
+	//var out bytes.Buffer
+	io.WriteString(w, mountPath)
+}
+
+func getContext() context.Context {
+	return context.TODO()
 }
 
 func handleRequests() {
